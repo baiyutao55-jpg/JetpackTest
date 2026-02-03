fd3fe3b3abf7cba443d1a6be9d71cc232013d59e
baiyutao <baiyutao55@gmail.com>
2026-02-03 10:23:14 +0800

    JetpackTest commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(platform("androidx.room:room-runtime:2.1.0"))
+    implementation("androidx.room:room-runtime")
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
